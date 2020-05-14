42c54b58542f43a7600aa24a9fa2c106340b93b5
ricardovazcorrea <ricardovazcorrea@gmail.com>
2020-05-14 12:41:35 -0300

    f

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // // Serve the static files from the React app
 // app.use(express.static(path.join(__dirname, 'admin/build')));
