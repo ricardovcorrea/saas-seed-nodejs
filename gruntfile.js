b62ef20825075d31fd16ae688ee84b34a62954a1
ricardovazcorrea <ricardovazcorrea@gmail.com>
2020-05-14 20:59:30 -0300

    Grunt build

---
+++ gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.initConfig({
+        exec: {
+            build_api: 'tsc',
+            build_admin: 'cd admin && yarn install && npm run build'
+        },
+        clean: {
+            build: ['dist', 'admin/build'],
+        },
+        copy: {
+            admin: {
+                cwd: 'admin/build',
+                src: ['**'],
+                dest: 'dist/admin/build',
+                expand: true
+            }
+        },
+        concurrent: {
+            build: {
+                tasks: ['exec:build_api', 'exec:build_admin'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('dev', ['concurrent:dev']);
+    grunt.registerTask('build', ['clean:build', 'concurrent:build', 'copy:admin']);
+
+};
