d70f8dcc278b376683be27e842eb76c6ac009e1a
ricardovazcorrea <ricardovazcorrea@gmail.com>
2020-05-14 12:51:12 -0300

    build admin

--- server.ts
+++ server.ts
@@ -4,17 +4,17 @@ import path from 'path';
 const app = express();
 const port = Number.parseInt(process.env.PORT) || 5000;
 
-// // Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, 'admin/build')));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'admin/build')));
 
 app.get('/teste', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
 
-// // Handles any requests that don't match the ones above
-// app.get('*', (req,res) =>{
-// 	res.sendFile(path.join(__dirname+'/admin/build/index.html'));
-// });
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/admin/build/index.html'));
+});
   
 app.listen(port, err => {
   console.log('teste');
